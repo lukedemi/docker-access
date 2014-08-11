47109ef8e46ab3c9ec7d6edbb49d41f08dc629d7
Luke Demi <luke@cloudmine.me>
2014-08-11 14:59:51 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 docker-access
 =============
 
-Python script to gain shell access to a docker container based on a container id or name (using nsenter)
+I write this guy to help me quickly get in and out of docker containers while I'm debugging environments. It's pretty simple - just call it with a container id or 'name' and it will open up the first match. I added in some simple argparse values in case you want to specifically check only ids or names, but I doubt they'll ever be useful.
+
+## Install
+
+Python Requirements:
+```
+sudo apt-get install python-pip
+pip install -r requirements.txt
+```
+Nsenter:
+```
+cd /tmp
+curl https://www.kernel.org/pub/linux/utils/util-linux/v2.24/util-linux-2.24.tar.gz \
+     | tar -zxf-
+cd util-linux-2.24
+./configure --without-ncurses
+make nsenter
+cp nsenter /usr/local/bin
+```
+
+## Usage
+
+```
+usage: docker_access.py [-h] [-i] [-n] search_query
+
+positional arguments:
+  search_query  Search query (id or name)
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -i            Will only search docker container ids
+  -n            Will only search docker container names
+```
---
+++ docker_access.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import docker
+import argparse
+from subprocess import call
+from distutils import spawn
+
+c = docker.Client(base_url='unix://var/run/docker.sock', version='1.12', timeout=10)
+
+if not spawn.find_executable('nsenter'):
+    print "Please install nsenter on your system before using this script"
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('search_query', help='Search query (id or name)')
+PARSER.add_argument('-i', dest='id', action='store_true',
+                     help='Will only search docker container ids')
+PARSER.add_argument('-n', dest='name', action='store_true',
+                     help='Will only search docker container names')
+RESULTS = PARSER.parse_args()
+
+def nsenter(container):
+    pid = c.inspect_container(container)['State']['Pid']
+    call('nsenter --target %s --mount --uts --ipc --net --pid' % pid, shell=True)
+
+for container in c.containers():
+    if RESULTS.name and RESULTS.search_query in container['Image']:
+        nsenter(container)
+        exit(0)
+    elif RESULTS.id and RESULTS.search_query in container['Id']:
+        nsenter(container)
+        exit(0)
+    elif not RESULTS.id or RESULTS.name and RESULTS.search_query in container['Id'] + container['Image']:
+        nsenter(container)
+        exit(0)
+
+print 'container not found.'
